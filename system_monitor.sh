618d61e874f2c17fa1e4d0f8a293f3ff8439e3ff
purnima202 <purnimajd20@gmail.com>
2026-09-04 09:36:56 +0000

    added monitoring logs

--- system_monitor.sh
+++ system_monitor.sh
@@ -3,11 +3,19 @@
 #This scripts monitors disk usage, memory usage and top resources consuming processes
 
 set -o pipefail
-echo "System Monitoring script starting...."
+LOG_FILE="system_monitoring.log"
+exec > >(tee -a "$LOG_FILE") 2>&1
+echo "======= System Monitoring Started: $(date '+%y-%m-%d %H:%M:%S') ======="
 
 #Disk Monitoring
 DISK_THRESHOLD=80
 disk_usage=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
+
+if [ $? -ne 0 ]; then
+	echo "ERROR: Failed to check disk usage"
+	exit 1
+fi
+
 echo "Disk Usage is : $disk_usage"
 
 if [ "$disk_usage" -ge "$DISK_THRESHOLD" ]; then
@@ -19,6 +27,12 @@ fi
 #Memory Monitoring
 MEMORY_THRESHOLD=80
 memory_usage=$(free | grep Mem | awk '{print ($3/$2*100)}' | cut -d. -f1)
+
+if [ $? -ne 0 ]; then
+	echo "ERROR: Failed to check memory usage"
+	exit 1
+fi
+
 echo "Memory Usage is : $memory_usage"
 
 if [ "$memory_usage" -ge "$MEMORY_THRESHOLD" ]; then
@@ -30,6 +44,12 @@ fi
 #CPU Monitoring
 CPU_THRESHOLD=80
 cpu_usage=$(top -bn1 | grep "Cpu" | awk '{print (100-$8)}' | cut -d. -f1)
+
+if [ $? -ne 0 ]; then
+	echo "ERROR: Failed to check cpu usage"
+	exit 1
+fi
+
 echo "CPU Usage is : $cpu_usage"
 
 if [ "$cpu_usage" -ge "$CPU_THRESHOLD" ]; then
