c3387cbfdac4a6811dc6481ab236f969e5a19c77
purnima <purnimajd20@gamil.com>
2026-09-04 12:36:02 +0530

    Added memory usage monitoring

--- system_monitor.sh
+++ system_monitor.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #system_monitor.sh
-#Thos scripts monitors disk usage, memory usage and top resources consuming processes
+#This scripts monitors disk usage, memory usage and top resources consuming processes
 
 set -o pipefail
 echo "System Monitoring script starting...."
 
+#Disk Monitoring
 DISK_THRESHOLD=80
 disk_usage=$(df -h | grep -v Filesystem | awk '{print $6}' | tr -d '%')
 echo "Disk Usage is : $disk_usage"
@@ -13,4 +14,15 @@ if [ "$disk_usage" -ge "$DISK_THRESHOLD" ]; then
     echo "ALERT: Disk usage is at ${disk_usage}% , threshold is at ${DISK_THRESHOLD}%"
 else
     echo "OK: Disk usage is at ${disk_usage}%"
+fi
+
+#Memory Monitoring
+MEMORY_THRESHOLD=80
+memory_usage=$(free | grep Mem | awk '{print ($3/$2*100)}' | cut -d. -f1)
+echo "Memory Usage is : $memory_usage"
+
+if [ "$memory_usage" -ge "$MEMORY_THRESHOLD" ]; then
+    echo "ALERT: Memory usage is at ${memory_usage}% , threshold is at ${MEMORY_THRESHOLD}%"
+else
+    echo "OK: Memory usage is at ${memory_usage}%"
 fi
